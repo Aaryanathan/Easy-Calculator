bae009d4c415a5c68ef4b18ea67eae809ebf9973
Aaryanathan <116165346+Aaryanathan@users.noreply.github.com>
2022-11-30 18:54:49 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+x = float(input("Enter Number: "))
+y = float(input("Enter Number: "))
+z = x - y
+a = x + y
+b = x * y
+c = x / y
+d = x % y
+print("The difference is", " ", z)
+print("The sum of the Numbers is", " ", a)
+print("The Product is", " ", b)
+print("The Quotient is", " ", b)
+print("The Remainder is ", d)
